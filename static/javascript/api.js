39f2639813799bc141088834a1be5b3a014e528a
mateiocristi <mateiocristi@gmail.com>
2021-12-17 22:37:21 +0200

    work on get products on the main page

--- static/javascript/api.js
+++
@@ -1,8 +0,0 @@
-async function apiGet(url) {
-    const request = await fetch(url);
-    if (request.ok) {
-        return request.json();
-    }
-    console.log('Error, pleas try again later');
-    alert('Error, pleas try again later');
-}
--- static/javascript/home_page.js
+++ static/javascript/home_page.js
@@ -1,6 +1,36 @@
+
+
 function init() {
-    let products_container = document.querySelector(`.products-container`)
-    get_products(products_container)
+    alert("ok")
+    apiGetAllProducts().then((response) => {
+        showProducts(response)
+    })
 }
 
 init();
+
+
+async function apiGetAllProducts() {
+    let category_id = 0
+    let supplier_id = 0
+    const products = await fetch(`/api_get_products/${category_id}/${supplier_id}`);
+    let json = products.json()
+    console.log("json" + json);
+    return await json;
+}
+
+export function showProducts(products) {
+    const products_container = document.querySelector(`.products-container`)
+    console.log("products: " + products[0])
+    console.log("container: " +products_container)
+    products_container.innerHTML = "";
+    for (let product of products) {
+        let imgPath = `/product_${product.id}.jpg`
+        products_container.insertAdjacentHTML("beforeend", `<div id="product_card">
+                                            <div id="product_content">
+                                                <h3>${product.name}</h3>
+                                                <div><img src="/static/images${imgPath}" alt=""></div>
+                                            </div>
+                                          </div>`)
+    }
+}
