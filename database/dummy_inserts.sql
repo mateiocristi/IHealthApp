b0f55f2b636aed3483b2bb4a34e6fe848478631d
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2021-12-17 20:38:13 +0200

    solved a bug in querries.py

--- database/dummy_inserts.sql
+++ database/dummy_inserts.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.carts (id, quantity_product_id) VALUES (default, null);
 INSERT INTO public.carts (id, quantity_product_id) VALUES (default, null);
 
-INSERT INTO public.users (id, first_name, last_name, county, city, address, birth_date, email, phone_number, cnp, username, password, access_level, cart_id) VALUES (default, 'Dragos', 'Doru', '', '', null, null, 'dragosdoru@email.com', null, null, 'dragosdoru', '$2b$12$RAtcHKrILl/KvjX9G97vg.YXJRwmsFDWI88WZdiTxGAHmVfqTP6hG', null, 1);
-INSERT INTO public.users (id, first_name, last_name, county, city, address, birth_date, email, phone_number, cnp, username, password, access_level, cart_id) VALUES (default, 'Valentin', 'Dragan', null, null, null, null, 'valentindragan@mail.ru', null, null, 'valentindragan', '$2b$12$RAtcHKrILl/KvjX9G97vg.YXJRwmsFDWI88WZdiTxGAHmVfqTP6hG', null, 2);
+INSERT INTO public.users (id, first_name, last_name, county, city, address, birth_date, email, phone_number, password, access_level, cart_id) VALUES (default, 'Dragos', 'Doru', '', '', null, null, 'dragosdoru@email.com', null, '$2b$12$RAtcHKrILl/KvjX9G97vg.YXJRwmsFDWI88WZdiTxGAHmVfqTP6hG', default, 1);
+INSERT INTO public.users (id, first_name, last_name, county, city, address, birth_date, email, phone_number, password, access_level, cart_id) VALUES (default, 'Valentin', 'Dragan', null, null, null, null, 'valentindragan@mail.ru', null, '$2b$12$RAtcHKrILl/KvjX9G97vg.YXJRwmsFDWI88WZdiTxGAHmVfqTP6hG', default, 2);
