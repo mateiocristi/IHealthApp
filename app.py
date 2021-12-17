701096fe23745bc58d31ed1889a4a2aefcbf69b2
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2021-12-17 17:27:36 +0200

    added querries.py functions

--- app.py
+++ app.py
@@ -15,29 +15,53 @@ def home():  # put application's code here
 @app.route("/api-get-products/<int: category_id>/<int: supplier_id>")
 @json_response
 def api_get_products(category_id, supplier_id):
-    product_1 = {"id": 1, "name": "product_1", "description": "some desc", "stock": 2, "default_price": 100.00,
-                "actual_price": 100.00, "rating": 2, "category_id": 1, "supplier_id": 2, "img_path": ""}
-    product_2 = {"id": 1, "name": "product_1", "description": "some desc", "stock": 2, "default_price": 100.00,
-                "actual_price": 100.00, "rating": 2, "category_id": 1, "supplier_id": 2, "img_path": ""}
+    product_1 = {
+        "id": 1,
+        "name": "product_1",
+        "description": "some desc",
+        "stock": 2,
+        "default_price": 100.00,
+        "actual_price": 100.00,
+        "rating": 2,
+        "category_id": 1,
+        "supplier_id": 2,
+        "img_path": "",
+    }
+    product_2 = {
+        "id": 1,
+        "name": "product_1",
+        "description": "some desc",
+        "stock": 2,
+        "default_price": 100.00,
+        "actual_price": 100.00,
+        "rating": 2,
+        "category_id": 1,
+        "supplier_id": 2,
+        "img_path": "",
+    }
     products = []
 
     if category_id == 0:
         if supplier_id == 0:
             products = querries.get_all_products()
         else:
-            products = [product for product in products if product["supplier_id"] == supplier_id]
+            products = [
+                product for product in products if product["supplier_id"] == supplier_id
+            ]
     else:
-        products = querries.get_products_with_category_id(category_id)
+        products = querries.get_products_by_category(category_id)
         if supplier_id != 0:
-            products = [product for product in products if product["supplier_id"] == supplier_id]
+            products = [
+                product for product in products if product["supplier_id"] == supplier_id
+            ]
     return products
 
 
-@app.route('/api-get-categories')
+@app.route("/api-get-categories")
 @json_response
 def api_get_categories():
     return querries.get_all_categories()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=8080, debug=True)
--- querries.py
+++ querries.py
@@ -2,6 +2,30 @@ import connection
 
 
 @connection.connection_handler
+def get_user_by_username(cursor, username):
+    querry = """
+        SELECT * 
+        FROM users
+        WHERE 
+            username = %(username)s
+        OR
+            email = %(username)s
+        
+            ;"""
+    cursor.execute(querry, {'username': username})
+
+
+@connection.connection_handler
+def get_user_by_id(cursor, user_id):
+    querry = """
+        SELECT * 
+        FROM users
+        WHERE id = %(user_id)s
+            ;"""
+    cursor.execute(querry, {'user_id': user_id})
+
+
+@connection.connection_handler
 def get_all_products(cursor):
     querry = """
             SELECT *
@@ -12,13 +36,35 @@ def get_all_products(cursor):
 
 
 @connection.connection_handler
-def get_products_with_category_id(cursor, category_id):
+def get_products_by_category(cursor, category_id):
     querry = """
                 SELECT *
                 FROM products
                 WHERE category_id = %(category_id)s
                 ;"""
     cursor.execute(querry, {"category_id": category_id})
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_products_by_supplier(cursor, supplier_id):
+    querry = """
+                SELECT *
+                FROM products
+                WHERE supplier_id = %(supplier_id)s
+                ;"""
+    cursor.execute(querry, {"supplier_id": supplier_id})
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_products_by_id(cursor, product_id):
+    querry = """
+                SELECT *
+                FROM products
+                WHERE id = %(product_id)s
+                ;"""
+    cursor.execute(querry, {"product_id": product_id})
     return cursor.fetchall()
 
 
@@ -30,3 +76,5 @@ def get_all_categories(cursor):
                 ;"""
     cursor.execute(querry)
     return cursor.fetchall()
+
+
