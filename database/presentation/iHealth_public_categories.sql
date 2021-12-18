c56752f58bbfc303807b45c6bbe8af8f370bbacd
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2021-12-18 15:43:48 +0200

    Inserts for presentation added

---
+++ database/presentation/iHealth_public_categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.categories (id, name, description) VALUES (3, 'Homeopatie', 'Dureri musculare, contuzii si echimoze.');
+INSERT INTO public.categories (id, name, description) VALUES (4, 'Raceala si gripa', 'Medicamente pentru tuse, raceala, gripa si dureri in gat');
+INSERT INTO public.categories (id, name, description) VALUES (5, 'Cosmetice si Ingrijire personala', 'Creme, lotiuni si tratamentele pentru ingrijirea fetei si a corpului');
+INSERT INTO public.categories (id, name, description) VALUES (6, 'Erotice', 'Potenta maxima!!!!!!');
+INSERT INTO public.categories (id, name, description) VALUES (7, 'Fertilitate', 'Tratamente pentru cresterea fertilitatii');
+INSERT INTO public.categories (id, name, description) VALUES (8, 'Imunitate', 'Ceaiuri si siropuri pentru cresterea imunitatii');
+INSERT INTO public.categories (id, name, description) VALUES (9, 'Prim-ajutor', 'Plasturi si bandaje');
